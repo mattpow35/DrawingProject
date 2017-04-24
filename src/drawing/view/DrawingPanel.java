9eb31d76cf484b6d5d0cb74facb989bafe9696b9
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-24 09:27:38 -0600

    setup the panel GUI and started to write methods to draw rectangles

--- src/drawing/view/DrawingPanel.java
+++ src/drawing/view/DrawingPanel.java
@@ -1,19 +1,27 @@
 package drawing.view;
 
-import javax.swing.JPanel;
-import javax.swing.SpringLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.*;
 import drawing.controller.DrawingController;
 
 public class DrawingPanel extends JPanel 
 {
 	private SpringLayout baseLayout;
+	private SpringLayout baseLayout_1;
 	private DrawingController baseController;
+	private JButton rectangleButton;
+	private ShapePanel shapePanel;
 	
 	public DrawingPanel(DrawingController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		this.baseLayout = new SpringLayout();
+		shapePanel = new ShapePanel(baseController);
+		rectangleButton = new JButton("Draw rectangles");
+		baseLayout_1 = new SpringLayout();
+		
 		
 		setupPanel();
 		setupLayout();
@@ -23,12 +31,22 @@ public class DrawingPanel extends JPanel
 	
 	private void setupPanel()
 	{
+		this.setLayout(baseLayout_1);
+		this.setBackground(Color.GRAY);
+		this.setPreferredSize(new Dimension(500, 500));
 		
+		this.add(rectangleButton);
+		this.add(shapePanel);
 	}
 	
 	private void setupLayout()
 	{
-		
+		baseLayout_1.putConstraint(SpringLayout.NORTH, rectangleButton, 0, SpringLayout.NORTH, shapePanel);
+		baseLayout_1.putConstraint(SpringLayout.WEST, rectangleButton, 10, SpringLayout.WEST, this);
+		baseLayout_1.putConstraint(SpringLayout.WEST, shapePanel, 216, SpringLayout.WEST, this);
+		baseLayout_1.putConstraint(SpringLayout.SOUTH, shapePanel, 490, SpringLayout.NORTH, this);
+		baseLayout_1.putConstraint(SpringLayout.NORTH, shapePanel, 10, SpringLayout.NORTH, this);
+		baseLayout_1.putConstraint(SpringLayout.EAST, shapePanel, -10, SpringLayout.EAST, this);
 	}
 	
 	private void setupListeners()
--- src/drawing/view/ShapePanel.java
+++ src/drawing/view/ShapePanel.java
@@ -1,8 +1,60 @@
 package drawing.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JPanel;
+import drawing.controller.DrawingController;
+import java.util.ArrayList;
+import java.awt.Rectangle;
 
 public class ShapePanel extends JPanel
 {
+	private DrawingController baseController;
+	private ArrayList<Rectangle> rectangleList;
+	
+	public ShapePanel(DrawingController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		rectangleList = new ArrayList<Rectangle>();
 
+		setupPanel();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.CYAN);
+		this.setMinimumSize(new Dimension(250,500));
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private Color getRandomColor()
+	{
+		int alpha = (int) (Math.random() * 255);
+		int red = (int) (Math.random() * 255);
+		int green = (int) (Math.random() * 255);
+		int blue = (int) (Math.random() * 255);
+		
+		return new Color(red, green, blue, alpha);
+	}
+	
+	public void addRectangles()
+	{
+		for(int index = 0; index < 500; index ++)
+		{
+			int width = (int) (Math.random() * 120) + 1;
+			int height = (int) (Math.random() * 150) + 15;
+			int xCorner = (int) (Math.random() * this.getWidth() - 15);
+			int yCorner = (int) (Math.random() * this.getHeight() - 15);
+			
+			Rectangle currentRectangle = new Rectangle(xCorner, yCorner, width, height);
+			rectangleList.add(currentRectangle);
+		}
+		this.repaint();
+	}
 }
